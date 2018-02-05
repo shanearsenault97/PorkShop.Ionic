6a9d3e7d6763d126411557dade7c8bd5cfbf64ac
Shane Arsenault <shane@thinkingbig.net>
2018-02-05 16:00:52 -0400

    removes an unused import

--- PorkShop/src/pages/reservation/reservation-add/reservation-add.ts
+++ PorkShop/src/pages/reservation/reservation-add/reservation-add.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {IonicPage} from "ionic-angular";
 import { IReservation } from '../../../models/interfaces/IReservation';
 import { ReservationService } from '../../../services/ReservationService';
